26bbe3e2ec774b3a5ad2706fc9861801afecc677
mspano <mspanopablo@gmail.com>
2023-07-13 12:52:00 -0300

    parte basica terminada

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,26 @@
 
-//dom
+//conectamos div para crear la card
 const contPoke = document.querySelector('.pokemonContainer')
 
 //funcion para consumir la api
 function takePokemon(numero){
-    fetch( `https://pokeapi.co/api/v2/pokemon/${numero}/`)
+    fetch( `https://pokeapi.co/api/v2/pokemon/`+ numero)
     .then((response) => response.json())
     .then((pokemon) => {
-        console.log(pokemon)
-        CreatePokemon(pokemon)
+        contPoke.innerHTML = `
+        <div class="pokemon-block"> 
+          <h1 class="name">#${pokemon.id.toString().padStart(4, 0)} - ${pokemon.name.toUpperCase()}</h1>
+          <img src="${pokemon.sprites.front_default}" />
+          <p>Weight: ${pokemon.weight} Lbs |
+          Height: ${pokemon.height}"<br>
+          HP: ${pokemon.stats[0].base_stat} | 
+          Type: ${pokemon.types.map(el=>el.type.name)}
+          </p>
+        </div>
+       `;
+       console.log(pokemon)
     });
 }
-
 
 function takePokemons(number){
     for( let i=1; i<=number; i++){
@@ -19,46 +28,12 @@ function takePokemons(number){
     }
 }
 
-
-
-//creamos la card pokemon
-
-function CreatePokemon(pokemon){
-
-    const card = document.createElement("div");
-    card.classList.add("pokemon-block")
-
-    const spriteContainer = document.createElement("div");
-    spriteContainer.classList.add("img-container");
-
-    const sprite = document.createElement("img");
-    sprite.src = pokemon.sprites.front_default;
-
-    spriteContainer.appendChild(sprite);
-
-    const number = document.createElement("p");
-    number.classList.add("number");
-    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
-
-    const name = document.createElement("p");
-    name.classList.add("name");
-    name.textContent = pokemon.name;
-
-    card.appendChild(spriteContainer);
-    card.appendChild(number);
-    card.appendChild(name);
-
-    contPoke.appendChild(card);
-
-}
-//  fetchPokemon(151)
-//  fetchPokemons(1)
-
+//buscamos por formulario el pokemon acorde a su numero
 const inputPoke = document.querySelector("#inputPoke");
 const formPoke = document.querySelector("#formPoke");
 
-//buscamos por formulario el pokemon acorde a su numero
 formPoke.addEventListener("submit", (event) =>{
     event.preventDefault();
     takePokemon(inputPoke.value)
+    
 });
