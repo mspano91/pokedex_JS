272886648a36f098247e723d84db8b85b2654be1
mspano <mspanopablo@gmail.com>
2023-07-19 20:13:04 -0300

    filters buttoms customized

--- JS/main.js
+++ JS/main.js
@@ -2,42 +2,48 @@
 //conectamos div para crear la card
 const contPoke = document.querySelector('.pokemonContainer')
 
-//funcion para consumir la api
+//funcion para crear la card
 function takePokemon(numero){
     fetch( `https://pokeapi.co/api/v2/pokemon/`+ numero)
     .then((response) => response.json())
     .then((pokemon) => {
         contPoke.innerHTML = `
-        <div class="pokemon-block"> 
-          <h1 class="name">#${pokemon.id.toString().padStart(4, 0)} - ${pokemon.name.toUpperCase()}</h1>
-          <img src="${pokemon.sprites.front_default}" />
-          <p>Weight: ${pokemon.weight} Lbs |
-          Height: ${pokemon.height}"<br>
-          HP: ${pokemon.stats[0].base_stat} | 
-          Type: ${pokemon.types.map(el=>el.type.name)} | <br>
-          Abilities: ${pokemon.abilities.map(el=>el.ability.name)}
-          </p>
+        <div class="pokemon-block">
+
+                 <h1 class="name">#${pokemon.id.toString().padStart(4, 0)} - ${pokemon.name.toUpperCase()}</h1>
+                 <img src="${pokemon.sprites.front_default}"/>
+
+            <div class="pokemon-skills">
+                    <p class="skills"> Weight: ${pokemon.weight}Lbs </p>
+                    <p class="skills"> Height: ${pokemon.height}"
+                    <p class="skills"> HP: ${pokemon.stats[0].base_stat}</p>
+                    <p class="skills"> Type: ${pokemon.types.map(el=>el.type.name)}</p>
+                    <p class="skills"> Ability: ${pokemon.abilities[0].ability.name}</p>
+            </div>
+
         </div>
        `;
-       console.log(pokemon)
+
     });
 }
 
 
-function takePokemons(number){
-    for( let i=1; i<=number; i++){
-        takePokemon(i);
-    }
-}
+
+// function takePokemons(number){
+//     for( let i=1; i<=number; i++){
+//         takePokemon(i);
+//     }
+// }
+
+
 
 //buscamos por formulario el pokemon acorde a su numero
 const inputPoke = document.querySelector("#inputPoke");
 const formPoke = document.querySelector("#formPoke");
-
 formPoke.addEventListener("submit", (event) =>{
     event.preventDefault();
     takePokemon(inputPoke.value)
-    
+
 });
 
 //boton seleccion random
@@ -47,8 +53,35 @@ btnSurprice.addEventListener("click",(event) => {
     let max = 1010;
     let min = 1;
     let random = Math.floor((Math.random() * (max - min + 1)) + min);
-    console.log(random);
     takePokemon(random)
-}
-   
-  )
+    })
+
+//funcion para traer pokemones random por tipo
+    function getRandomTypePokemon(type) {
+        let max = 1010;
+        let min = 1;
+        let random = Math.floor((Math.random() * (max - min + 1)) + min);
+      
+            return fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+            .then((response) => response.json())
+            .then((pokemon) => {
+                if (pokemon.types.some(el => el.type.name === type)) {
+                return takePokemon(random);
+                } else {
+                return getRandomTypePokemon(type);
+                }
+          });
+      }
+
+ // Función para manejar el evento click en los botones
+      function handleButtonClick(event) {
+        event.preventDefault();
+        const type = event.target.id;
+        getRandomTypePokemon(type);
+      }
+
+      const buttons = document.querySelectorAll(".id-btn");
+      buttons.forEach(button => button.addEventListener("click", handleButtonClick));
+
+
+
